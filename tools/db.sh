a430ecda6046624397e2c53fc4cb0d5619838466
fabiofilipe <fabiofmf21@gmail.com>
2026-04-21 03:19:04 -0300

    chore(tools): remove scaffolds SQLite substituidos por TypeScript/Postgres

--- tools/db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Executa uma query SQL no banco do agente
-# Uso: db.sh "SELECT * FROM user_context WHERE chat_id = '5511999999999@s.whatsapp.net'"
-DB_PATH="${AGENT_DB_PATH:-/data/agent.db}"
-sqlite3 "$DB_PATH" "$1"
